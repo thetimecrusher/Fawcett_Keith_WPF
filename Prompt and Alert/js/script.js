cbae2aa40225e68967d07f3b5e6099622896484f
thetimecrusher <keithfawcett@Home-Base.local>
2015-04-03 16:25:04 -0700

    Completed the PromptsAndAlerts Video
    I Completed the PromptsAndAlerts Video in the Expressions Assignment.

---
+++ Prompt and Alert/js/script.js
@@ -0,0 +1,13 @@
+/**
+ * Created by keithfawcett on 4/3/15.
+ */
+
+/*var userInput = prompt("Enter your year of birth:");
+console.log(userInput);*/
+
+var width = prompt("We are calculating the area of a rectangle.\nPlease enter the width");
+var height = prompt("Please enter the height.");
+var area = width * height; //calculates with that info
+//console.log(area); //prints it out to console
+var result = "The area of you rectangle is " + area+ " sq feet";
+alert(result);
