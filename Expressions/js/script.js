b56d1f8ee770b7c71ba7162a72f7e5b57398cac8
thetimecrusher <keithfawcett@Home-Base.local>
2015-04-03 15:20:50 -0700

    Completed ExampleExpression Video
    I Completed the ExampleExpression Video in the Expressions Assignment.

--- Expressions/js/script.js
+++ Expressions/js/script.js
@@ -2,7 +2,6 @@
  * Created by keithfawcett on 4/3/15.
  */
 
-var a = 2;//sets up our variable a and defines it with 2
-var b;
-b = a + 3;//add 3 to a
-console.log(a);
+var yearBorn = 1994;
+var age = 2015 - yearBorn -1;
+console.log(age);
