4774adae3ef3cfa60afc62ba118d0124fa86764a
thetimecrusher <keithfawcett@Home-Base.local>
2015-04-03 16:50:52 -0700

    Completed Dog Years Problem
    I wrote the code and made the appropriate comments for the Dog Years
    Problem.

---
+++ Week 2 Activities/Expressions_worksheet/js/script.js
@@ -0,0 +1,10 @@
+// Keith Fawcett 4/3/15 Expressions_Worksheet
+
+
+//Calculate age of a dog in dog years
+
+var age = prompt("How old is sparky?"); //user inputs the dogs age
+var dogAge = Number(age) * 7; //Multiplies the dogs age by 7 to get the age in dog years
+
+//Prints to the console the age of the dog in human years and in dog years
+console.log("Sparky is " + age  + " human years old which is " + dogAge + " in dog years.")
