0df8b7cca10c595192c6d8f83abdb8eb99b9bbb1
thetimecrusher <keithfawcett@Home-Base.local>
2015-04-03 16:03:34 -0700

    Completed AssignmentOperators Video
    I Completed the AssignmentOperators Video in the Expressions Assignment.

--- Expressions/js/script.js
+++ Expressions/js/script.js
@@ -2,14 +2,7 @@
  * Created by keithfawcett on 4/3/15.
  */
 
-/*var stringVar = "6";
-var result = 7 + Number(stringVar);
-console.log(result);
-    */
-
-var areaCode = 407;
-var firstPart = 203;
-var secPart = 4529;
-
-var phoneNo = "(" + String(areaCode)+")" + String(firstPart) +"-" + String(secPart);
-console.log(phoneNo);
+var a = 3;
+//a ++; // a = a + 1 // a += 1
+a --; // a = a - 1 // a -= 1
+console.log(a);
