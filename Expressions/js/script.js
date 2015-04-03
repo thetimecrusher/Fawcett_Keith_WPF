fdf815590d413947bcab26b75a9eb71f24636af3
thetimecrusher <keithfawcett@Home-Base.local>
2015-04-03 15:39:22 -0700

    Completed OrderOfOperations Video
    I Completed the OrderOfOperations Video in the Expressions Assignment.

--- Expressions/js/script.js
+++ Expressions/js/script.js
@@ -2,5 +2,16 @@
  * Created by keithfawcett on 4/3/15.
  */
 
-var remainder = 9 % 2;
-console.log(remainder);
+var quiz1 = 87;
+var quiz2 = 100;
+var quiz3 = 60;
+var quiz4 = 80;
+
+//the average is the sum of the items divided by number of items
+var average = (quiz1 + quiz2 + quiz3 + quiz4)/4;
+//console.log(average);
+
+var length = 7;
+var width = 6;
+var perimeter = length * 2 + width * 2;
+console.log(perimeter);
