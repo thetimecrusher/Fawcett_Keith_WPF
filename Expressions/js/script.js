4344757a49e8933b68d56f6ffcf8678a0b7140c9
thetimecrusher <keithfawcett@Home-Base.local>
2015-04-03 15:31:56 -0700

    Completed Modulo Video
    I Completed the Modulo Video in the Expressions Assignment.

--- Expressions/js/script.js
+++ Expressions/js/script.js
@@ -2,8 +2,5 @@
  * Created by keithfawcett on 4/3/15.
  */
 
-//area of the triangle is half of the width times the height
-var width = 8;
-var height = 7;
-var area = width * height/2;
-console.log(area);
+var remainder = 9 % 2;
+console.log(remainder);
