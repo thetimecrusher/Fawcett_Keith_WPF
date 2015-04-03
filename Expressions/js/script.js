b5b61766173fbcfde3c9ea4de7de129e2e2119a6
thetimecrusher <keithfawcett@Home-Base.local>
2015-04-03 15:42:46 -0700

    Completed ArraysInAnExpression Video
    I Completed the ArraysInAnExpression Video in the Expressions
    Assignment.

--- Expressions/js/script.js
+++ Expressions/js/script.js
@@ -2,16 +2,7 @@
  * Created by keithfawcett on 4/3/15.
  */
 
-var quiz1 = 87;
-var quiz2 = 100;
-var quiz3 = 60;
-var quiz4 = 80;
+var orangeBins =[234, 567, 883];
 
-//the average is the sum of the items divided by number of items
-var average = (quiz1 + quiz2 + quiz3 + quiz4)/4;
-//console.log(average);
-
-var length = 7;
-var width = 6;
-var perimeter = length * 2 + width * 2;
-console.log(perimeter);
+var total = orangeBins[0] + orangeBins[1] +orangeBins[2];
+console.log(total);
