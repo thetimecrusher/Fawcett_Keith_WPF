7e4f74a571a9af2ed4e700654dd17c1162236b1f
thetimecrusher <keithfawcett@Home-Base.local>
2015-04-03 17:07:27 -0700

    Completed Slice of Pie part 1 Problem
    I wrote the code and made the appropriate comments for the Slice of Pie
    part 1 Problem.

--- Week 2 Activities/Expressions_worksheet/js/script.js
+++ Week 2 Activities/Expressions_worksheet/js/script.js
@@ -1,5 +1,6 @@
 // Keith Fawcett 4/3/15 Expressions_Worksheet
 
+/*
 
 //Calculate age of a dog in dog years
 
@@ -7,4 +8,16 @@ var age = prompt("How old is sparky?"); //user inputs the dogs age
 var dogAge = Number(age) * 7; //Multiplies the dogs age by 7 to get the age in dog years
 
 //Prints to the console the age of the dog in human years and in dog years
-console.log("Sparky is " + age  + " human years old which is " + dogAge + " in dog years.")
+console.log("Sparky is " + age  + " human years old which is " + dogAge + " in dog years.");
+
+*/
+
+//Calculates how much pizza each person will get
+var NumPizza = prompt("How many pizzas were ordered?"); //user inputs the number of pizzas ordered
+var slicesPerPizza = prompt("How many slices are the pizzas cut into?"); //user inputs the number of slices each pizza has
+var people = prompt("How many people are at the party?") //user inputs the number of people at the party
+
+var slicesPerPerson = NumPizza * slicesPerPizza / people; // Calculates how many slices each person can have
+
+//Prints to the console the number of slices that each person at the party had
+console.log("Each person ate " + slicesPerPerson + " slices of pizza at the party.");
