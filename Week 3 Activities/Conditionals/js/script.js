8b9d5d750bd0d18214a979a2168972f618b4e73d
thetimecrusher <keithfawcett@Home-Base.local>
2015-04-06 16:47:16 -0700

    Completed Condition with Expression Video
    Watched and completed Condition with Expression Video

--- Week 3 Activities/Conditionals/js/script.js
+++ Week 3 Activities/Conditionals/js/script.js
@@ -2,12 +2,13 @@
  * Created by keithfawcett on 4/6/15.
  */
 
-var kidHeight = 40;
+var kidHeight = 47;
 var minHeight = 48;
+var sneakerLift =2;
 
 //if the child is old enough, print to the console "you can ride!"
 //if the kid is over 48 inches in height
-if(kidHeight > minHeight){
+if(kidHeight + sneakerLift > minHeight){
     //code performed if condition is true
     console.log("You can ride the coaster!");
 }
