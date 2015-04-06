9ede1c63544c1ee21d6fc1fe02863584cf708ff1
thetimecrusher <keithfawcett@Home-Base.local>
2015-04-06 16:58:06 -0700

    Completed Else if Video
    Watched and completed Else if Video

--- Week 3 Activities/Conditionals/js/script.js
+++ Week 3 Activities/Conditionals/js/script.js
@@ -4,12 +4,17 @@
 
 var kidHeight = 52;
 var minHeight = 48;
+var wParentHeight = 45;
 
 //if the child is old enough, print to the console "you can ride!"
 //if the kid is over 48 inches in height
 if(kidHeight > minHeight){
-    //code performed if condition is true
+    //you can ride!
     console.log("You can ride the coaster!");
+}else if(kidHeight > wParentHeight){
+    //you can ride with a parent present
+    console.log("You can ride, but only with a parent present.");
 }else{
+    //sorry you have growing to do
     console.log("Sorry kid, you've got some growing to do first!");
 }
