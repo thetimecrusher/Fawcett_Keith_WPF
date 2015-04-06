263a775d65c38e7a4f7daf49445d13953ec40c4c
thetimecrusher <keithfawcett@Home-Base.local>
2015-04-06 16:52:42 -0700

    Completed If and Else Video
    Watched and completed If and Else Video

--- Week 3 Activities/Conditionals/js/script.js
+++ Week 3 Activities/Conditionals/js/script.js
@@ -2,13 +2,14 @@
  * Created by keithfawcett on 4/6/15.
  */
 
-var kidHeight = 47;
+var kidHeight = 52;
 var minHeight = 48;
-var sneakerLift =2;
 
 //if the child is old enough, print to the console "you can ride!"
 //if the kid is over 48 inches in height
-if(kidHeight + sneakerLift > minHeight){
+if(kidHeight > minHeight){
     //code performed if condition is true
     console.log("You can ride the coaster!");
+}else{
+    console.log("Sorry kid, you've got some growing to do first!");
 }
