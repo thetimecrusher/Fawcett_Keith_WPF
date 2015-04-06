149a769fe7bd3cb4b01283e6a608e4f274803c46
thetimecrusher <keithfawcett@Home-Base.local>
2015-04-06 02:27:04 -0700

    Created Month Array
    I created an array that holds every month in the year.

--- Week 2 Activities/Fawcett_Keith_Expressions_Assignment/js/script.js
+++ Week 2 Activities/Fawcett_Keith_Expressions_Assignment/js/script.js
@@ -7,3 +7,8 @@ var price = prompt("What did it cost?"); //user inputs how much their item costs
 var interest = prompt("What percent interest is your loan?"); //user inputs the percent that their loan is
 var paymentYear = prompt("What year do you want your " + item + " payed off by?"); //user inputs what year they want their item payed off
 var paymentMonth = prompt("What month?\nEnter 1-12"); //user inputs what month they want their item pay off
+
+paymentMonth --; // changes the input from starting at 1 to starting at 0 like arrays use
+
+//logs the months in an array
+var month = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
