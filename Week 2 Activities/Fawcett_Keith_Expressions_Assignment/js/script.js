fcbf49abcaf7a251582066acdb3856b1a6d3d8ed
thetimecrusher <keithfawcett@Home-Base.local>
2015-04-06 02:25:51 -0700

    Prompts completed
    I created all the variables that will receive the user input from the
    prompts.

--- Week 2 Activities/Fawcett_Keith_Expressions_Assignment/js/script.js
+++ Week 2 Activities/Fawcett_Keith_Expressions_Assignment/js/script.js
@@ -1,1 +1,9 @@
 // Keith Fawcett 4/6/15 Expressions_Assignment
+
+//Calculates the monthly payment needed for a loan to be payed off by the users desired date
+
+var item = prompt("What did you buy?"); //user inputs the item that they bought
+var price = prompt("What did it cost?"); //user inputs how much their item costs
+var interest = prompt("What percent interest is your loan?"); //user inputs the percent that their loan is
+var paymentYear = prompt("What year do you want your " + item + " payed off by?"); //user inputs what year they want their item payed off
+var paymentMonth = prompt("What month?\nEnter 1-12"); //user inputs what month they want their item pay off
