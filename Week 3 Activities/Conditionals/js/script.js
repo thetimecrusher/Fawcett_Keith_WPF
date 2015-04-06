6a65cfac031230c0d19cb282caa394d0bd2c5622
thetimecrusher <keithfawcett@Home-Base.local>
2015-04-06 16:39:59 -0700

    Completed Conditionals Examples
    Watched conditionals video. Watched and completed conditionals examples
    videos.

--- Week 3 Activities/Conditionals/js/script.js
+++ Week 3 Activities/Conditionals/js/script.js
@@ -2,11 +2,11 @@
  * Created by keithfawcett on 4/6/15.
  */
 
-if(sunny){
-    goToTheBeach();
-    if(warmWater){
-        wearNewSuit();
-    }
-}else{
-    goToTheMovies();
+var oldEnough = false;
+
+//if the child is old enough, print to the console "you can ride!'
+if(oldEnough){
+    //code performed if condition is true
+    console.log("You can ride the coaster!");
 }
+console.log("What comes after");
