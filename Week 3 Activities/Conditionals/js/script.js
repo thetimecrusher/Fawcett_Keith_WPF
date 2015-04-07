5d962cf2af86fb97a9d6166f2fac7e8be2ce8cdd
thetimecrusher <keithfawcett@Home-Base.local>
2015-04-06 18:30:45 -0700

    Completed Logical Operators Video
    Watched and completed the Logical Operators Video

--- Week 3 Activities/Conditionals/js/script.js
+++ Week 3 Activities/Conditionals/js/script.js
@@ -2,19 +2,13 @@
  * Created by keithfawcett on 4/6/15.
  */
 
-var kidHeight = 52;
-var minHeight = 48;
-var wParentHeight = 45;
+var budget = 100;
+var iPhonePrice = 199.99;
+var wonLottery = true;
 
-//if the child is old enough, print to the console "you can ride!"
-//if the kid is over 48 inches in height
-if(kidHeight > minHeight){
-    //you can ride!
-    console.log("You can ride the coaster!");
-}else if(kidHeight > wParentHeight){
-    //you can ride with a parent present
-    console.log("You can ride, but only with a parent present.");
+//if the price of the phone is less then our budget AND if our paycheck is over 300
+if(iPhonePrice < budget || wonLottery){
+    console.log("We can buy the phone!");
 }else{
-    //sorry you have growing to do
-    console.log("Sorry kid, you've got some growing to do first!");
+    console.log("No phone for you!");
 }
