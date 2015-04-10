4ab0e4240b44158b3b8d4c0748cb0bba91cf3d08
thetimecrusher <keithfawcett@Home-Base.local>
2015-04-10 04:46:24 -0700

    Prompts Created
    I created the starting variables and prompts that collects the users
    inputs.

--- Week 3 Activities/Fawcett_Keith_Conditionals_Assignment/js/script.js
+++ Week 3 Activities/Fawcett_Keith_Conditionals_Assignment/js/script.js
@@ -1,1 +1,8 @@
 // Keith Fawcett 4/10/15 Conditionals Assignment
+
+//user input variables
+var holes = prompt("Are you going to do 9, or 18 holes?"); //user inputs number of holes they will play
+var cart = prompt("Would you like to rent a cart?"); //user inputs if they want to rent a cart
+var clubs = prompt("Would you like to rent clubs?"); //user inputs if they want to rent clubs
+var month = prompt("What month will you be playing?"); //user inputs the month they will be playing
+var time = prompt("What time would you like to play?"); //user inputs the time they will be playing
