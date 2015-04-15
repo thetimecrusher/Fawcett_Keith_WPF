b8254b84f53dfb287aeec938ccfde3658e1e205d
thetimecrusher <keithfawcett@Home-Base.local>
2015-04-14 17:39:46 -0700

    Final Edits
    I added some user friendly items like being able to type y for yes, and
    added some forgotten semicolons and capitalization.

--- Week 3 Activities/Fawcett_Keith_Conditionals_Assignment/js/script.js
+++ Week 3 Activities/Fawcett_Keith_Conditionals_Assignment/js/script.js
@@ -7,24 +7,24 @@ if(holes === "9" || holes === "18") { //if the input was 9 or 18
     holes = prompt("Please choose either 9 or 18."); //ask the user to input 9 or 18 again
 }
 var cart = prompt("Would you like to rent a cart? \nYES or NO"); //user inputs if they want to rent a cart
-if(cart.toUpperCase() === "YES" || cart.toUpperCase() === "NO") { //if the input was yes or no
+if(cart.toUpperCase() === "YES" || cart.toUpperCase() === "NO" || cart.toUpperCase() === "Y" || cart.toUpperCase() ==="N") { //if the input was yes or no
 }else{ //do nothing otherwise
     cart = prompt("Please answer YES or NO"); //ask the user to input yes or no again
 }
 var clubs = prompt("Would you like to rent clubs? \nYES or NO"); //user inputs if they want to rent clubs
-if(clubs.toUpperCase() === "YES" || clubs.toUpperCase() === "NO") { //if the input was yes or no
+if(clubs.toUpperCase() === "YES" || clubs.toUpperCase() === "NO" || clubs.toUpperCase() === "Y" || clubs.toUpperCase() === "N")  { //if the input was yes or no
 }else{ //do nothing otherwise
     clubs = prompt("Please answer YES or NO"); //ask the user to input yes or no again
 }
-var month = prompt("What month will you be playing?"); //user inputs the month they will be playing
+var month = prompt("What Month will you be playing?"); //user inputs the month they will be playing
 if(month.toUpperCase() === "JANUARY" || month.toUpperCase() === "FEBRUARY" || month.toUpperCase() === "MARCH" || month.toUpperCase() === "APRIL" || month.toUpperCase() === "MAY" || month.toUpperCase() === "JUNE" || month.toUpperCase() === "JULY" || month.toUpperCase() === "AUGUST" || month.toUpperCase() === "SEPTEMBER" || month.toUpperCase() === "OCTOBER" || month.toUpperCase() === "NOVEMBER" || month.toUpperCase() === "DECEMBER") { // if the input was a valid month
 }else{ //do nothing otherwise
-    month = prompt("Please enter a valid month."); //ask the user to input a valid month
+    month = prompt("Please enter a valid Month."); //ask the user to input a valid month
 }
-var time = prompt("What hour would you like to play?\nPlease enter in military time between 6 and 18."); //user inputs the time they will be playing
-if(time >= "6" || time <= "18") { //if the time is between 6 and 18
+var time = prompt("What Hour would you like to play?\nPlease enter using a 24 hour clock between 6 and 18."); //user inputs the time they will be playing
+if(time >= 6 && time <= 18) { //if the time is between 6 and 18
 }else{ //do nothing otherwise
-    time = prompt("Please chose a military hour between 6 and 18.") //ask the user to input a number between 6 and 18 again
+    time = prompt("Please chose between 6 and 18."); //ask the user to input a number between 6 and 18 again
 }
 
 
@@ -43,14 +43,14 @@ if(holes === "18"){ //if the user is playing 18 holes
 if(month.toUpperCase() === "OCTOBER" || month.toUpperCase() === "NOVEMBER" || month.toUpperCase() === "DECEMBER" ||month.toUpperCase() === "JANUARY" || month.toUpperCase() === "FEBRUARY"){ //if the user is playing between october and february
     totalCost /= 2; //cut the totalCost in half
 }else if(month.toUpperCase() === "MARCH" || month.toUpperCase() === "APRIL" || month.toUpperCase() === "MAY"){ //if the user is playing between march and may
-    totalCost *= 1 //keep the totalCost the same
+    totalCost *= 1; //keep the totalCost the same
 }else{ //otherwise if the user is playing between june and september
-    totalCost *= 2 //double the totalCost
+    totalCost *= 2; //double the totalCost
 }
 
-(cart.toUpperCase() === "YES") ? cart = 15: cart = 0; //if the user is getting a cart make cart $15 otherwise make it 0
+(cart.toUpperCase() === "YES" || "Y") ? cart = 15: cart = 0; //if the user is getting a cart make cart $15 otherwise make it 0
 
-(clubs.toUpperCase() === "YES") ? clubs = 15: clubs = 0; // if the user is renting clubs make the clubs $15 otherwise make it 0
+(clubs.toUpperCase() === "YES" || "Y") ? clubs = 15: clubs = 0; // if the user is renting clubs make the clubs $15 otherwise make it 0
 
 if(time >= 6 && time < 15){ //if the user is playing between 6am and 3pm
     time = 15; //make time $15
