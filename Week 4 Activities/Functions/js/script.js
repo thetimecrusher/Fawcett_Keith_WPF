7f84bf7f3c8dd19845f77baeff46c740d902a1af
thetimecrusher <keithfawcett@Home-Base.local>
2015-04-14 18:42:21 -0700

    Completed the Variable Scope Video
    Watched and completed the Variable Scope Video

--- Week 4 Activities/Functions/js/script.js
+++ Week 4 Activities/Functions/js/script.js
@@ -2,17 +2,14 @@
  * Created by keithfawcett on 4/14/15.
  */
 
-function outptMsg(){
-    console.log("Hello World");
-}
+var width = 5;
 
 function calcArea(){
     var width = 20;
     var height = 30;
     var area = width * height;
-    console.log(area);
+    //console.log(area);
 }
 
 calcArea();
-calcArea();
-calcArea();
+console.log(width);
