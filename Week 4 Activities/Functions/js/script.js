4ce2fb3a696405174e2d0735a460ca54877da2df
thetimecrusher <keithfawcett@Home-Base.local>
2015-04-14 17:47:40 -0700

    Completed Basic Functions Video
    Watched the Introduction to Functions video and the Different types of
    Functions video.
    Watched and completed Basic Functions Structure Video

---
+++ Week 4 Activities/Functions/js/script.js
@@ -0,0 +1,14 @@
+/**
+ * Created by keithfawcett on 4/14/15.
+ */
+
+function outptMsg(){
+    console.log("Hello World");
+}
+
+function calcArea(){
+    var width = 20;
+    var height = 30;
+    var area = width * height;
+    console.log(area);
+}
