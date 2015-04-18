8dad3f315aefa02fb391fa891d910328e4568eee
thetimecrusher <keithfawcett@Home-Base.local>
2015-04-18 01:18:02 -0700

    Created the area function
    Created the area function

--- Week 4 Activities/Functions_worksheet/js/script.js
+++ Week 4 Activities/Functions_worksheet/js/script.js
@@ -8,3 +8,5 @@
 
 var width = prompt("What is the width of your Rectangle?"); //user inputs the width of their rectangle
 var height = prompt("What is the height of your Rectangle?"); //user inputs the height of their rectangle
+
+function calculateArea(w, h){ //makes the functions calculateArea
