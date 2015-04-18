ba945ca7cfed1cb72b02bf8500c3f1309433e305
thetimecrusher <keithfawcett@Home-Base.local>
2015-04-18 01:17:14 -0700

    Prompt for the height
    Prompt for the height

--- Week 4 Activities/Functions_worksheet/js/script.js
+++ Week 4 Activities/Functions_worksheet/js/script.js
@@ -7,3 +7,4 @@
 //Calculate the Area of a Rectangle
 
 var width = prompt("What is the width of your Rectangle?"); //user inputs the width of their rectangle
+var height = prompt("What is the height of your Rectangle?"); //user inputs the height of their rectangle
